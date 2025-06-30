00af307d93fa03d830565bf12b0aca4d9d5c85f5
Girish <110347535+Ftyigffifygf@users.noreply.github.com>
2025-06-30 11:59:08 +0530

    Delete Human_Genome_Loader/install.sh

--- Human_Genome_Loader/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "🔧 Installing requirements..."
-pip install biopython tqdm matplotlib
-
-echo "📁 Creating genome_data folder..."
-mkdir -p genome_data
-
-echo "✅ All set! Now run: python main.py"
